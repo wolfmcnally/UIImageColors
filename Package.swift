4a918ecb01ab5eee5da03bef2ecb8494c4c255c6
Wolf McNally <wolf@wolfmcnally.com>
2019-10-20 14:42:15 -0700

    Updated for Xcode 11.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,15 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
-  name: "UIImageColors"
+    name: "UIImageColors",
+    platforms: [
+        .iOS(.v9), .macOS(.v10_13), .tvOS(.v11)
+    ],
+    products: [
+        .library(name: "UIImageColors", targets: ["UIImageColors"])
+    ],
+    targets: [
+        .target(name: "UIImageColors", path: "UIImageColors")
+    ]
 )
